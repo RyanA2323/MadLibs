2b70e48cab4bcbfc72ca46be2f5dd96028f3c18d
Ryan <ryan.atkinson21@d155.org>
2020-11-09 20:11:14 -0600

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by Tiger Coder on 11/9/20.
+//  Copyright © 2020 clc.Atkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
